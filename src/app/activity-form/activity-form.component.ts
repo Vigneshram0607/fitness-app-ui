fcb3bba75c34ef9f6d4c5c56db0f2897bf6e7aa7
Vigneshram0607 <vigneshram0706@gmail.com>
2026-01-07 20:49:56 +0530

    added activity form skeleton

---
+++ src/app/activity-form/activity-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-form.component.html',
+  styleUrl: './activity-form.component.scss'
+})
+export class ActivityFormComponent {
+
+}
