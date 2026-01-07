b38504e20944828ee7e05b913b6ee12863bb4a16
Vigneshram0607 <vigneshram0706@gmail.com>
2026-01-07 20:50:48 +0530

    Added activity list and form in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivityFormComponent } from "../activity-form/activity-form.component";
+import { ActivityListComponent } from "../activity-list/activity-list.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ActivityFormComponent, ActivityListComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
