093f7ca7c1830c50c51a2669ba2808936c2387c6
Vigneshram0607 <vigneshram0706@gmail.com>
2026-01-07 20:51:01 +0530

    added redirect to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+    {path: '', redirectTo: 'home',pathMatch: 'prefix'},{
     path:'home',
     component: DashboardComponent
 }];
