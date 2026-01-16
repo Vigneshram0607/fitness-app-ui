757de76b513286759d23e22bb4f32f0902d900c5
Vigneshram0607 <vigneshram0706@gmail.com>
2026-01-16 11:57:09 +0530

    added edge case routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ActivityDetailComponent } from './activity-detail/activity-detail.component';
 
 export const routes: Routes = [
-    {path:'', redirectTo: 'activity', pathMatch: 'full'},
+    
     {path: 'activity', component: DashboardComponent, pathMatch: 'full'},
-    {path: 'activity/:activityId', component: ActivityDetailComponent, pathMatch: 'full'}
+    {path: 'activity/:activityId', component: ActivityDetailComponent, pathMatch: 'full'},
+    {path: '', redirectTo: 'activity', pathMatch: 'full'},{path:'**', redirectTo: 'activity', pathMatch: 'full'},
 ];
 
 
