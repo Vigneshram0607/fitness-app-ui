be87ea59419495078af0aef39d35d6dc4be80335
Vigneshram0607 <vigneshram0706@gmail.com>
2026-01-16 12:54:09 +0530

    synchronised dashboard data with activity stats

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,40 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivityListComponent } from "../activity-list/activity-list.component";
 import { ActivityFormComponent } from '../activity-form/activity-form.component';
+import { DashboardStatsService } from '../services/dashboard-stats.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ActivityListComponent, ActivityFormComponent],
+  imports: [ActivityListComponent, ActivityFormComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
-  @ViewChild(ActivityListComponent) activityListComponent!: ActivityListComponent;
+export class DashboardComponent implements OnInit, OnDestroy {
+  totalCaloriesBurned: number = 0;
+  totalWorkouts: number = 0;
+  avgDuration: number = 0;
+  calorieChangePercentage: number = 0;
+  
+  private statsSubscription!: Subscription;
 
-  onActivityAdded() {
-    // this.activityListComponent.fetchActivities();
+  constructor(private dashboardStatsService: DashboardStatsService){}
+
+  ngOnInit() {
+    this.statsSubscription = this.dashboardStatsService.stats$.subscribe(stats => {
+      this.totalCaloriesBurned = stats.totalCaloriesBurned;
+      this.totalWorkouts = stats.totalWorkouts;
+      this.avgDuration = stats.avgDuration;
+      this.calorieChangePercentage = stats.calorieChangePercentage;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.statsSubscription) {
+      this.statsSubscription.unsubscribe();
+    }
   }
 }
 
