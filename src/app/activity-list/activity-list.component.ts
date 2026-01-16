9c0c1a723fd526517d9c7dfeeabee344c27c03a6
Vigneshram0607 <vigneshram0706@gmail.com>
2026-01-16 09:00:15 +0530

    Optimise activity llisting

--- src/app/activity-list/activity-list.component.ts
+++ src/app/activity-list/activity-list.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-activity-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './activity-list.component.html',
   styleUrl: './activity-list.component.scss'
 })
-export class ActivityListComponent {
+export class ActivityListComponent implements OnInit {
+  activities: any[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.loadActivities();
+    this.apiService.activityAdded$.subscribe(() => {
+      this.loadActivities();
+    });
+  }
+
+  loadActivities(): void {
+    this.apiService.getActivities().subscribe(data => {
+      if (data) {
+        this.activities = data;
+        this.activities.sort((a, b) => {
+          // Convert strings to Date objects
+          const dateA = new Date(a.createdAt).getTime();
+          const dateB = new Date(b.createdAt).getTime();
+
+          // Descending order (newest first)
+          return dateB - dateA;
+        });
+      }
+    });
+  }
+
+  getIconClass(activityType: string): string {
+    switch (activityType) {
+      case 'RUNNING':
+        return 'fas fa-running fa-lg';
+      case 'CYCLING':
+        return 'fas fa-bicycle fa-lg';
+      case 'SWIMMING':
+        return 'fas fa-swimmer fa-lg';
+      case 'YOGA':
+        return 'fas fa-child fa-lg';
+      default:
+        return 'fas fa-dumbbell fa-lg';
+    }
+  }
 
 }
